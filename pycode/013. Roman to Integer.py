e658e739a437e1c44aad2484edd1d39d30a9bdb7
4disland <dileu12@163.com>
2016-09-12 20:20:56 +0800

    add 13

--- pycode/013. Roman to Integer.py
+++ pycode/013. Roman to Integer.py
@@ -4,8 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        syms = [I, V, X, L, C, D, M]
+        syms = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
         val = [1, 5, 10, 50, 100, 500, 1000]
 
         num = 0
+        for i in range(len(s)-1):
+            idx1, idx2 = map(syms.index, (s[i], s[i+1]))
+            if idx1 >= idx2:
+                num += val[idx1]
+            else:
+                num -= val[idx1]
+        num += val[syms.index(s[-1])]
+        return num
+
+
+tests = ['IV', 'III', 'II', 'V', 'VI']
+for t in tests:
+    print(Solution().romanToInt(t))
+            
+
         
