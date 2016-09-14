2e256ee87a755d76d3ba2a5265f1941b6be707b9
4disland <dileu12@163.com>
2016-09-14 15:31:14 +0800

    modify 389

--- pycode/389. Find the Difference.py
+++ pycode/389. Find the Difference.py
@@ -5,9 +5,22 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        for c in s:
-             t = t.replace(c, '', 1) # only remove the first c in t
-        return t
+        # method 1
+        # for c in s:
+        #      t = t.replace(c, '', 1) # only remove the first c in t
+        # return t
+
+        # method 2
+        for c in s+t:
+            if (s+t).count(c) % 2:
+                return c
+
+        # method 3
+        # code = 0
+        # for c in s+t:
+        #     code ^= ord(c)
+        # return chr(code)
+        
 
 a = Solution().findTheDifference('abcd', 'dcabe')
 print(a)
