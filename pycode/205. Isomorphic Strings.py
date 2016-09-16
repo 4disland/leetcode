bb94f88ba90d4b43f2990eda4633fe7635061d36
4disland <dileu12@163.com>
2016-09-16 21:53:53 +0800

    add 205

---
+++ pycode/205. Isomorphic Strings.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        d1, d2 = [0 for _ in xrange(256)], [0 for _ in xrange(256)]
+        for i in xrange(len(s)):
+            if d1[ord(s[i])] != d2[ord(t[i])]:
+                return False
+            d1[ord(s[i])] = i+1
+            d2[ord(t[i])] = i+1
+        return True
